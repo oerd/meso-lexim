84ad00a89e5ef1bdb9fe9f76de1ac31b8e6ac505
Oerd Cukalla <rosaccu@gmail.com>
2021-01-29 19:55:28 +0100

    fix tests

--- src/tests/header.test.tsx
+++ src/tests/header.test.tsx
@@ -4,9 +4,9 @@ import { shallow } from "enzyme";
 import Header from "../components/header";
 
 describe("Initial Test of the Header", () => {
-    test("Header renders 3 nav items", () => {
+    test("Header renders 4 nav items", () => {
         const context = shallow(<Header />);
         expect(context.find("h1").text()).toBe("Preact App");
-        expect(context.find("Link").length).toBe(3);
+        expect(context.find("Link").length).toBe(4);
     });
 });
